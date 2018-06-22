2ec2623e70a76cd5a7ef98dca041977f994fed25
Benoit Lormeau <benoit.lormeau@epitech.eu>
2018-06-23 01:42:06 +0200

    Corrected NotFound sentence

--- views/Error404/index.php
+++ views/Error404/index.php
@@ -1,6 +1,6 @@
 <div class="jumbotron">
 	<h1>404 Not Found</h1>
-	<p>It seems like to trying to access some page that don't or doesn't exists anymore.</p>
+	<p>It seems like you are trying to access some page that don't or doesn't exists anymore.</p>
 	<br>
 
 	<a href="<?= WEBROOT ?>" class="btn btn-lg btn-primary">Homepage</a>
