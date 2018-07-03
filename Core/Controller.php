93ce2ff56a83e8eb7a2de73d1be5155a41d853c2
Benoit Lormeau <benoit.lormeau@epitech.eu>
2018-07-03 16:28:35 +0200

    Added custom file loading (styles/scripts)

--- Core/Controller.php
+++ Core/Controller.php
@@ -4,19 +4,20 @@ namespace Core;
 
 class Controller
 {
-	private $vars = [];
-	private $view;
 	protected $model;
+	private $view;
+	private $vars = [];
+	private $files = [];
 
 	public function __construct()
 	{}
 
-	public function loadModel($model)
+	public function loadModel($model, ...$args)
 	{
 		require_once(ROOT . "App/Models/$model.php");
 
 		$name = "\\App\\Models\\$model";
-		$this->model = new $name();
+		$this->model = new $name(...$args);
 	}
 
 	public function setVars($vars)
@@ -24,11 +25,39 @@ class Controller
 		$this->vars = array_merge($this->vars, $vars);
 	}
 
+	public function loadFile($file)
+	{
+		if (is_array($file))
+			$this->files = array_merge($this->files, $file);
+		else
+			$this->files = array_merge($this->files, [$file]);
+	}
+
 	public function render($filename, $layout = 'default')
 	{
 		$parts = explode('\\', get_class($this));
+
 		$this->view = new View(end($parts), $layout);
+		$this->passFilesToView();
 		$this->view->setVars($this->vars);
 		$this->view->render($filename);
+	}
+
+	private function passFilesToView()
+	{
+		foreach ($this->files as $file) {
+			$filepath = ROOT . "public/$file";
+			$ext = pathinfo($filepath, PATHINFO_EXTENSION);
+			switch ($ext) {
+				case 'css':
+					$this->view->loadStyle($file);
+					break;
+				case 'js':
+					$this->view->loadScript($file);
+					break;
+				default:
+					break;
+			}
+		}
 	}
 }
--- Core/View.php
+++ Core/View.php
@@ -7,6 +7,10 @@ class View
 	private $view;
 	private $layout;
 	private $vars = [];
+	private $files = [
+		'styles' => [],
+		'scripts' => []
+	];
 
 	public function __construct($controllerName, $layout = 'default')
 	{
@@ -19,12 +23,23 @@ class View
 		$this->vars = array_merge($this->vars, $vars);
 	}
 
+	public function loadStyle($style)
+	{
+		$this->files['styles'][] = WEBROOT . $style;
+	}
+
+	public function loadScript($script)
+	{
+		$this->files['scripts'][] = WEBROOT . $script;
+	}
+
 	public function render($filename)
 	{
-		extract($this->vars);
-
 		$_controller = $this->view;
 		$_action = ucfirst($filename);
+
+		extract($this->vars);
+		extract($this->files);
 
 		require_once(ROOT . 'App/Views/layouts/' . $this->layout . '/header.php');
 		Session::renderFlash();
