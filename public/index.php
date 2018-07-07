974bb1b6425d63cb21dc4b3469e4ec50a2b2ba51
Benito <swimaster61@live.fr>
2018-07-07 20:23:24 +0200

    Fix include path

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
 define('ROOT',    str_replace('public/index.php', '', $_SERVER['SCRIPT_FILENAME']));
 
-require_once(ROOT . "core/Autoload.php");
+require_once(ROOT . "Core/Autoload.php");
 
 ///////////////////////////////////////////////////////////////////////////////
 
